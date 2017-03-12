c851e99eda5d3ae7200f68416379790bfad3c2f0
cd C:\RailsInstaller\Ruby2.2.0\bin <copy rake.bat rails.bat>
2017-03-12 14:47:06 +0100

    Main page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :submissions
   post '/accept/:id',to:'submissions#accept'
+  root to: "submissions#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
